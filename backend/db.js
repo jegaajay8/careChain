15fe15ddc022fa674bcdf98cf9ab3453ccee1041
jegaajay8 <jegaajay8@gmail.com>
2026-03-07 17:54:46 +0530

    database added

---
+++ backend/db.js
@@ -0,0 +1,18 @@
+const mysql = require("mysql2");
+
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "YOUR_MYSQL_PASSWORD",
+    database: "carechain"
+});
+
+db.connect((err) => {
+    if (err) {
+        console.log("Database error");
+    } else {
+        console.log("Database connected");
+    }
+});
+
+module.exports = db;
