2f08d76f3e5b7a192dbd912cabf905f509225795
Lasanthan-Km <lasanthan21122004@gmail.com>
2026-03-19 20:54:55 +0530

    updated backend files and frontend files

--- backend/db.js
+++ backend/db.js
@@ -9,7 +9,7 @@ const db = mysql.createConnection({
 
 db.connect((err) => {
     if (err) {
-        console.log("Database error");
+        console.log("Database error:", err);
     } else {
         console.log("Database connected");
     }
