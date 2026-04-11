1f454eff5982ac788bb6d97b765973d5c3540ab9
unknown <kabilraj.24@cse.mrt.ac.lk>
2026-04-11 10:32:12 +0530

    update data base

--- backend/db.js
+++ backend/db.js
@@ -4,6 +4,7 @@ const mysql = require("mysql2");
 // Using a Pool is better for handling multiple donor/hospital requests at once
 const db = mysql.createPool({
     host: process.env.DB_HOST,
+    
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
