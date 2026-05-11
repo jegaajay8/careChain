beb4f7ae27aac289c779ff5010306f53ba6f2e98
jegaajay8 <jegaajay8@gmail.com>
2026-05-11 23:58:40 +0530

    donor and hospital table added

--- database/schema.sql
+++ database/schema.sql
@@ -18,3 +18,33 @@ CREATE TABLE IF NOT EXISTS users (
     role ENUM('donor', 'hospital', 'admin') NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+-- C. Donors Table
+CREATE TABLE IF NOT EXISTS donors (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    fullname VARCHAR(100) NOT NULL,
+    nic VARCHAR(20) UNIQUE NOT NULL,
+    telephone VARCHAR(20),
+    blood_group VARCHAR(5),
+    district VARCHAR(50),
+    city VARCHAR(50),
+    road VARCHAR(100),
+    postal_code VARCHAR(20),
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
+
+-- D. Hospitals Table (Profile)
+CREATE TABLE IF NOT EXISTS hospitals (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    rep_name VARCHAR(100) NOT NULL, 
+    hospital_name VARCHAR(100) NOT NULL,
+    hospital_id VARCHAR(50) NOT NULL, 
+    district VARCHAR(50) NOT NULL,
+    lat DECIMAL(10, 8) NOT NULL, 
+    lng DECIMAL(11, 8) NOT NULL,
+    city VARCHAR(50) NULL,       
+    road VARCHAR(100) NULL,      
+    postal_code VARCHAR(20) NULL, 
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
